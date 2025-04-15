2594498b83132bb6bbd7514d03f3c82b1ab96189
Ayan Khan <ayan.psit@gmail.com>
2025-04-15 11:02:42 +0000

    some changes

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+// This example show us a false practise of code readability below
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Ayan")
+
+
+let name = "Ayan"
+let age = 22
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
